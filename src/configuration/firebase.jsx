abaa520f8c94ca4862124f976eff2f70bde5fc14
rajaseyadu <98040368+seyaduraja@users.noreply.github.com>
2024-06-09 12:54:31 +0530

    Update firebase.jsx

--- src/configuration/firebase.jsx
+++ src/configuration/firebase.jsx
@@ -4,13 +4,7 @@ import {getAuth , GoogleAuthProvider} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ41VkOpCqxY7HosdVHgGW22hPNF7ceDA",
-  authDomain: "hanifa-motors.firebaseapp.com",
-  projectId: "hanifa-motors",
-  storageBucket: "hanifa-motors.appspot.com",
-  messagingSenderId: "635046286482",
-  appId: "1:635046286482:web:5b31afa9f49fae1fe6824b",
-  measurementId: "G-Q8LPX7NYKX"
+
 };
 
 
